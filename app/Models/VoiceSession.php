37a005f3fea7fc34dbdc4414b2ed029f11650c5c
jurecerkez-code <jure.cerkez@code.berlin>
2025-09-30 12:39:57 +0200

    feat: create VoiceSession model with fillable attributes and trip relationship

---
+++ app/Models/VoiceSession.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VoiceSession extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'trip_id',
+        'audio_file',
+        'duration',
+    ];
+    
+    public function trip()
+    {
+        return $this->belongsTo(Trip::class);
+    }
+}
