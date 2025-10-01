b31dcfcac216c71de763a11395ea97b9a51b6b91
jurecerkez-code <jure.cerkez@code.berlin>
2025-10-01 13:28:35 +0200

    style: fix namespace and use statements formatting in Comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\\Models;
+namespace App\Models;
 
-use Illuminate\\Database\\Eloquent\\Factories\\HasFactory;
-use Illuminate\\Database\\Eloquent\\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
