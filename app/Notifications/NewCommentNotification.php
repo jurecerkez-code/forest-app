9c47c71629b6e7e55f57b13c7225aed4363a6565
jurecerkez-code <jure.cerkez@code.berlin>
2025-10-01 14:50:08 +0200

    newcomment notification

--- app/Notifications/NewCommentNotification.php
+++ app/Notifications/NewCommentNotification.php
@@ -2,8 +2,8 @@
 
 namespace App\Notifications;
 
+use App\Models\Comment;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
@@ -11,44 +11,23 @@ class NewCommentNotification extends Notification
 {
     use Queueable;
 
-    /**
-     * Create a new notification instance.
-     */
-    public function __construct()
+    public function __construct(public Comment $comment)
     {
-        //
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @return array<int, string>
-     */
     public function via(object $notifiable): array
     {
         return ['mail'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     */
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(object $notifiable): array
-    {
-        return [
-            //
-        ];
+            ->subject('New Comment on Your Trip')
+            ->greeting('Hello ' . $notifiable->name . '!')
+            ->line($this->comment->user->name . ' commented on your meditation trip.')
+            ->line('"' . $this->comment->content . '"')
+            ->action('View Trip', route('trips.show', $this->comment->trip_id))
+            ->line('Thank you for being part of our community!');
     }
 }
